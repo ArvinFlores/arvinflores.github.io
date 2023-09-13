0ed55eae8cb2ae3638c70060f2700a315cd6a313
Arvin <arvin.flores@metalab.csun.edu>
2023-09-13 12:44:47 -0700

    Add work section

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './styles/base.css';
 import './styles/util.css';
+import './styles/button.css';
 import './styles/navbar.css';
 import './styles/hero.css';
 import './styles/about.css';
-import './styles/button.css';
+import './styles/work.css';
