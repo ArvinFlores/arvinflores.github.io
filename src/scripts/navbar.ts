e8fb91435b818efc098e6e6eb682eb3d7eee4a6e
Arvin <arvin.flores@metalab.csun.edu>
2023-09-13 18:28:30 -0700

    Made mobile nav menu

--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -3,8 +3,25 @@ export function initNavbar (navbar: HTMLElement | null): void {
 
   const height = getComputedStyle(navbar).height;
   const nextEl = navbar.nextElementSibling;
+  const menu = navbar.querySelector('.navbar__menu');
+  const menuBtn = document.getElementById('navbar__menu-btn');
+  const menuCloseBtn = document.getElementById('navbar__menu-close-btn');
 
   if (nextEl) {
     (nextEl as HTMLElement).style.marginTop = height;
+  }
+
+  if (menu && menuBtn && menuCloseBtn) {
+    menuBtn.addEventListener('click', () => {
+      menu.classList.add('navbar__menu--expanded');
+      menuBtn.setAttribute('disabled', 'true');
+      menuBtn.setAttribute('aria-expanded', 'true');
+    });
+
+    menuCloseBtn.addEventListener('click', () => {
+      menu.classList.remove('navbar__menu--expanded');
+      menuBtn.removeAttribute('disabled');
+      menuBtn.setAttribute('aria-expanded', 'false');
+    });
   }
 }
